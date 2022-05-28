d955c1b0b47a847e5b5a6f0e7c48ffeb8835e0f8
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-05-29 00:52:21 +0300

    Init

---
+++ build.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eo pipefail
+
+function process_openapi {
+  openapi-generator generate \
+  -i v1.openapi.yaml \
+  -o genned-api-for/$1 \
+  -g $1
+}
+
+rm -rf genned-api-for
+
+process_openapi haskell

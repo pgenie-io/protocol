6de28513aa5c4731dd5c38187e589a6ba1d593f6
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-19 19:45:57 +0300

    Add a golden test for deserialization

--- test/TestSuites/Serialization.hs
+++ test/TestSuites/Serialization.hs
@@ -12,5 +12,12 @@ initializeTests :: IO [TestTree]
 initializeTests =
   return
     [ testEncodeDecode @Protocol.Request Proxy,
-      testEncodeDecode @Protocol.Response Proxy
+      testEncodeDecode @Protocol.Response Proxy,
+      testCase "Deserialization golden" $
+        let input = "\NUL\ETX\STXmy\EOTuser\ENQspace\STX\ENQmusic\tcatalogue\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL\NUL"
+            value =
+              Protocol.ProcessRequest $
+                Protocol.RequestProcess "my-user-space" "music-catalogue" [] []
+         in assertEqual "" (Right value) $
+              Cereal.runGet @Protocol.Request Cereal.get input
     ]
